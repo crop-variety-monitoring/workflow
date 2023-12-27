77f15c71697765a53b89c3e27932644bff7f4450
rhijmans <r.hijmans@gmail.com>
2023-12-26 17:46:51 -0800

    d

--- 3_run_dartassign.R
+++ 3_run_dartassign.R
@@ -17,11 +17,9 @@ counts_file = ff = ff[3]
 for (counts_file in ff) {
 	print(dart_file)
 	info.file <- gsub("SNP_Counts.csv$", "genotype-info.csv", dart_file)
+
+
 	x = runSampleAnalysis(counts.file, info.file)
 
-	filename <- file.path(gsub("input", "output/IBS", dirname(dart_file)), snps$order)
-	out <- matchpoint::match_IBS(snps$snp, genotypes, markers, filename=filename, threads=4)
+	filename <- file.path(gsub("input", "output/DartAssign", dirname(dart_file)), snps$order)
 }
-
-#MAF_cutoff=0.05; SNP_Missing_Rate=0.2; Ref_Missing_Rate=0.2; Sample_Missing_Rate=0.2; Ref_Heterozygosity_Rate = 0.1; Sample_Heterozygosity_Rate=0.1; IBS_cutoff=0.5; Inb_method = "mom.visscher"; threads=1; verbose=FALSE; filename=""
-
